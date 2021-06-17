4ee4498cf2f5ede1959eeebb50c0b5cad829e14f
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2021-06-17 13:51:22 -0500

    Update visualization.py

--- superscreen/visualization.py
+++ superscreen/visualization.py
@@ -1,7 +1,11 @@
+from typing import Union, Tuple
+
 import numpy as np
 
 
-def auto_range_iqr(data_array, cutoff_percentile=90):
+def auto_range_iqr(
+    data_array: np.ndarray, cutoff_percentile: Union[float, Tuple[float, float]] = 90
+) -> Tuple[float, float]:
     """Get the min and max range of the provided array that excludes outliers
     following the IQR rule.
 
@@ -11,12 +15,13 @@ def auto_range_iqr(data_array, cutoff_percentile=90):
     https://github.com/QCoDeS/Qcodes/blob/master/qcodes/utils/plotting.py.
 
     Args:
-        data_array (np.ndarray): Array of arbitrary dimension containing the statistical data.
-        cutoff_percentile (float | tuple[float]): Percentile of data that may maximally be clipped
-            on both sides of the distribution. If given a tuple (a,b) the percentile limits will be a and 100-b.
+        data_array: Array of arbitrary dimension containing the statistical data.
+        cutoff_percentile (float | tuple[float]): Percentile of data that may
+            maximally be clipped on both sides of the distribution. If given
+            a tuple (a, b), the percentile limits will be a and 100-b.
 
     Returns:
-        (tuple[float]): (vmin, vmax)
+        vmin, vmax
     """
     if isinstance(cutoff_percentile, tuple):
         t = cutoff_percentile[0]
@@ -29,18 +34,14 @@ def auto_range_iqr(data_array, cutoff_percentile=90):
     zmin = np.nanmin(z)
     zrange = zmax - zmin
     pmin, q3, q1, pmax = np.nanpercentile(z, [b, 75, 25, 100 - t])
-    IQR = q3 - q1
-    # handle corner case of all data zero, such that IQR is zero
-    # to counter numerical artifacts do not test IQR == 0, but IQR on its
-    # natural scale (zrange) to be smaller than some very small number.
-    # also test for zrange to be 0.0 to avoid division by 0.
-    # all This is possibly to careful...
-    if zrange == 0.0 or IQR / zrange < 1e-8:
+    iqr = q3 - q1
+
+    if zrange == 0.0 or iqr / zrange < 1e-8:
         vmin = zmin
         vmax = zmax
     else:
-        vmin = max(q1 - 1.5 * IQR, zmin)
-        vmax = min(q3 + 1.5 * IQR, zmax)
+        vmin = max(q1 - 1.5 * iqr, zmin)
+        vmax = min(q3 + 1.5 * iqr, zmax)
         # do not clip more than cutoff_percentile:
         vmin = min(vmin, pmin)
         vmax = max(vmax, pmax)
