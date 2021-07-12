4b43946b302c0fb1606c0e4f4af83655a848ad6d
loganbvh <logan.bvh@gmail.com>
2021-07-12 09:34:47 -0500

    Remove license info from top of each file

--- superscreen/__init__.py
+++ superscreen/__init__.py
@@ -1,15 +1,10 @@
-# This file is part of superscreen.
-#
-#     Copyright (c) 2021 Logan Bishop-Van Horn
-#
-#     This source code is licensed under the MIT license found in the
-#     LICENSE file in the root directory of this source tree.
-
+from .about import version_dict, version_table
 from .brandt import convert_field, solve, solve_many
 from .device import Layer, Polygon, Device
 from .io import save_solutions, load_solutions
 from .parameter import Parameter, Constant
 from .solution import BrandtSolution
+from .version import __version__, __version_info__
 from .visualization import (
     auto_grid,
     grids_to_vecs,
@@ -24,4 +19,3 @@ from . import fem
 from . import geometry
 from . import parallel
 from . import sources
-from .version import __version__, __version_info__
