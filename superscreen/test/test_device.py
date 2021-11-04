c16db01f8f4db604907cc7d590273082f27ce28f
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2021-11-04 09:43:08 -0700

    Update tests

--- superscreen/test/test_device.py
+++ superscreen/test/test_device.py
@@ -24,6 +24,14 @@ def device():
         sc.Polygon("ring", layer="layer1", points=sc.geometry.ellipse(3, 2, angle=5)),
     ]
 
+    offset_film = films[0].offset_points(1)
+    assert isinstance(offset_film, np.ndarray)
+    assert offset_film.shape[0] >= films[0].points.shape[0]
+
+    offset_poly = films[0].offset_points(1, as_polygon=True)
+    assert isinstance(offset_poly, sc.Polygon)
+    assert films[0].name in offset_poly.name
+
     assert films[0].contains_points([0, 0])
     assert np.isclose(films[0].area, np.pi * 5 ** 2, rtol=1e-3)
     assert np.isclose(films[1].area, np.pi * 3 * 2, rtol=1e-3)
