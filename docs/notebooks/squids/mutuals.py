3a2bde1c23a09206885e90790a5406c47c8610fb
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2023-05-14 21:56:38 -0700

    Update mutuals.py

--- docs/notebooks/squids/mutuals.py
+++ docs/notebooks/squids/mutuals.py
@@ -5,20 +5,6 @@ import superscreen as sc
 
 from . import huber, hypres, ibm
 
-
-def get_mutual(squid, label, iterations, fc_lambda=None):
-    if fc_lambda is not None:
-        squid.layers["BE"].london_lambda = fc_lambda
-    print(squid)
-    # fluxoid_polys = sc.make_fluxoid_polygons(squid)
-    # fig, ax = squid.plot()
-    # for name, poly in fluxoid_polys.items():
-    #     ax.plot(*sc.geometry.close_curve(poly).T, label=name + "_fluxoid")
-    # ax.legend(bbox_to_anchor=(1, 1))
-    # ax.set_title(label)
-    return squid.mutual_inductance_matrix(iterations=iterations, units="Phi_0 / A")
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
